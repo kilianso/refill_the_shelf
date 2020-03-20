5edf8ea1eb3a3b4e4e45b3a6a266dee3a06d4381
Unknown <hello@kilianso.com>
2020-03-20 23:57:22 +0100

    add a language switch + i18n logic

---
+++ src/components/LangSelector.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { locale, locales, translations, getBrowserLocale } from 'svelte-intl';
+
+    translations.update({
+        de: {
+            brand: 'Füll das Regal',
+            layer: 'Ebene'
+        },
+        en: {
+            brand: 'Refill the Shelf',
+            layer: 'layer'
+        },
+    })
+    
+    locale.set(getBrowserLocale('en')) // try to use window.navigator.language, fallback to EN
+    const setLocale = e => locale.set(e.target.value)
+
+</script>
+
+<select value={$locale} on:change={setLocale}>
+  {#each $locales as l}
+    <option value={l}> {l} </option>
+  {/each}
+</select>
