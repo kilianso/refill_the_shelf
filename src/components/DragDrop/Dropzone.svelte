462a9b8e19e9004638fd4a763441745c8a0f0ffa
Unknown <hello@kilianso.com>
2020-03-24 18:50:58 +0100

    save and restore dropzone userLayer

--- src/components/DragDrop/Dropzone.svelte
+++ src/components/DragDrop/Dropzone.svelte
@@ -1,11 +1,22 @@
 <script>
-    let dropzones = [0,1,2,3];
+    import Item from './Item.svelte';
+    import { userLayer, layerPrice } from '../../store';
+
+    let dropzones = [0,1,2,3],
+        nonReactiveStoreCopy = $userLayer;
+        // this is needed to refill the dropzone after route change as soon as the component is mounted.
+        // but we don't want to listen to store changes once the user interacts with the draggables, 
+        // since this would lead to conflicts with the logic in Creation.svelte
 </script>
 <style lang="scss" global>
     @import './Dropzone.scss';
 </style>
 <div class="grid dropzone">
     {#each dropzones as {dropzone}, i}
-        <div class="droppable" data-position={i}></div>
+        <div class="droppable {nonReactiveStoreCopy[i].price ? 'taken' : ''}" data-position={i}>
+            {#if nonReactiveStoreCopy[i].price}
+                <Item price={nonReactiveStoreCopy[i].price} icon={nonReactiveStoreCopy[i].icon} alt={nonReactiveStoreCopy[i].alt} />
+            {/if}
+        </div>
     {/each}
 </div>
